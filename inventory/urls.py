8b9e26d78e757e39a2286a24506203be7d8cfe40
Shahriar <shovo.bd98@gmail.com>
2022-05-31 19:35:21 +0600

    fix url

--- inventory/urls.py
+++ inventory/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from inventory import views
 urlpatterns = [
     path('show-purchase/', views.PurchaseList.as_view()),
-    path('purchase-edit-delete-update/', views.PurchaseDetail.as_view()),
+    path('purchase-edit-delete-update/<int:pk>/', views.PurchaseDetail.as_view()),
 ]
