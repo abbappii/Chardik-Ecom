c09c830297a00d5f1a3cf36a15309ef0e71c7401
abbappii <bappi42434@gmail.com>
2022-05-31 15:21:07 +0600

    inventory serializer added

---
+++ inventory/serializer.py
@@ -0,0 +1,45 @@
+
+
+'''
+this file contains Serializers -
+
+Supplier Serializer
+Purchase Serializer
+'''
+
+# import section 
+from rest_framework import serializers
+from inventory.models import (
+    Supplier, Purchase
+)
+
+
+# serializer class 
+
+# supplier serializer 
+class SupplierSerializers(serializers.ModelSerializer):
+    class Meta:
+        model = Supplier
+        fields = ['id','name','description','country']
+    
+# purchase serailizer 
+class PurchaseSerialiers(serializers.ModelSerializer):
+    class Meta:
+        model = Purchase
+        fields = [
+            'id',
+            'ref_code',
+            'batch_no',
+            'date_of_purchase',
+            'order_status',
+            'product',
+            'price',
+            'supplier',
+            'outlet_name',
+            'unit_cost',
+            'other_cost',
+            'due_price',
+            'payment_method',
+            'payment_status',
+            'description'
+        ]
