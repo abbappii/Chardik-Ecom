84f91f6e83a96d8e7cbaa723ac320650283faf57
Shahriar <shovo.bd98@gmail.com>
2022-06-01 00:03:58 +0600

    URL empty

--- inventory/urls.py
+++ inventory/urls.py
@@ -1,6 +1,1 @@
-from django.urls import path
-from inventory import views
-urlpatterns = [
-    path('show-purchase/', views.PurchaseList.as_view()),
-    path('purchase-edit-delete-update/<int:pk>/', views.PurchaseDetail.as_view()),
-]
+
