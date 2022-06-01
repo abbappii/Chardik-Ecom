2f673fe3c12e43b61b5a3d347c27ccf27de004b0
abbappii <bappi42434@gmail.com>
2022-06-02 01:46:44 +0600

    products view recorrection

--- products/views/prodcuts_logic.py
+++ products/views/prodcuts_logic.py
@@ -12,64 +12,18 @@ from rest_framework.response import Response
 from rest_framework import status
 from rest_framework.parsers import MultiPartParser, FileUploadParser,FormParser
 from utils.util import *
-from products.database.products import Products,Product_images,ProductAttribute
+from products.database.products import Products 
 from products.serializers import *
-from rest_framework.renderers import TemplateHTMLRenderer
-from django.shortcuts import get_object_or_404
-
-
+ 
 
 #ProductsView
-# class ProductListViewSet(generics.ListAPIView):
-#     queryset = Products.objects.all()
-#     serializer_class = ProductsSerializers
-
-# class ProductCreateView(APIView):
-#     parser_classes = (MultiPartParser, FormParser)
-
-#     def post(self, request,format=None):
-#         data = request.data
-#         serializer = ProductsSerializers(data=data)
-        
-#         if serializer.is_valid():
-#             X =  serializer.save()
-#             product_images = dict((request.data).lists())['product_image']
-#             for image in product_images:
-#                 modified_data = Util.modify_input_for_multiple_files(image)
-#                 file_serializer = Product_imagesSerializer(data=modified_data)
-#                 if file_serializer.is_valid():
-#                     file_serializer.save(product=X)
-              
-#             return Response(serializer.data, status=status.HTTP_200_OK)
-#         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-
-# class ProductRetUpDesViewSet(generics.RetrieveUpdateDestroyAPIView):
-#     queryset = Products.objects.all()
-#     serializer_class = ProductsSerializers
-#     lookup_field = 'slug'
-
-
-# rendrer html form 
-class ProductListViewSet(APIView):
-    renderer_classes = [TemplateHTMLRenderer]
-    template_name = 'products/product_list.html'
-
-    def get(self, request):
-        queryset = Products.objects.all()
-        return Response({'products': queryset})
-
-
 
 class ProductListViewSet(generics.ListAPIView):
     queryset = Products.objects.all()
     serializer_class = ProductsSerializers
-   
+
 class ProductCreateView(APIView):
     parser_classes = (MultiPartParser, FormParser)
-
-    renderer_classes = [TemplateHTMLRenderer]
-    template_name = 'products/product_create.html'
 
     def post(self, request,format=None):
         data = request.data
@@ -83,24 +37,72 @@ class ProductCreateView(APIView):
                 file_serializer = Product_imagesSerializer(data=modified_data)
                 if file_serializer.is_valid():
                     file_serializer.save(product=X)
+              
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
-            return Response({'serializer': serializer})
-        return redirect({'products'})
 
-class ProductDetail(APIView):
-    renderer_classes = [TemplateHTMLRenderer]
-    template_name = 'products/product_detail.html'
+class ProductRetUpDesViewSet(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Products.objects.all()
+    serializer_class = ProductsSerializers
+    lookup_field = 'slug'
 
-    def get(self, request, slug):
-        product = get_object_or_404(Products, slug=slug)
-        serializer = ProductsSerializers(product)
-        return Response({'serializer': serializer, 'profile': product})
 
-    def post(self, request, slug):
-        product = get_object_or_404(Products, slug=slug)
-        serializer = ProductsSerializers(product, data=request.data)
-        if not serializer.is_valid():
-            return Response({'serializer': serializer, 'product': product})
-        serializer.save()
-        return redirect('products')
+# rendrer html form 
+# class ProductListViewSet(APIView):
+#     renderer_classes = [TemplateHTMLRenderer]
+#     template_name = 'products/product_list.html'
+
+#     def get(self, request):
+#         queryset = Products.objects.all()
+#         return Response({'products': queryset})
+
+
+
+# class ProductListViewSet(generics.ListAPIView):
+#     queryset = Products.objects.all()
+#     serializer_class = ProductsSerializers
+   
+# class ProductCreateView(APIView):
+#     parser_classes = (MultiPartParser, FormParser)
+ 
+#     def post(self, request,format=None):
+#         data = request.data
+#         serializer = ProductsSerializers(data=data)
+        
+#         if serializer.is_valid():
+#             X =  serializer.save()
+#             product_images = dict((request.data).lists())['product_image']
+#             for image in product_images:
+#                 modified_data = Util.modify_input_for_multiple_files(image)
+#                 file_serializer = Product_imagesSerializer(data=modified_data)
+#                 if file_serializer.is_valid():
+#                     file_serializer.save(product=X)
+
+#             return Response(serializer.data, status=status.HTTP_200_OK)
+#         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+
+# class ProductRetUpDesViewSet(generics.RetrieveUpdateDestroyAPIView):
+#     queryset = Products.objects.all()
+#     serializer_class = ProductsSerializers
+#     lookup_field = 'slug'
+
+
+# class ProductDetail(APIView):
+#     renderer_classes = [TemplateHTMLRenderer]
+#     template_name = 'products/product_detail.html'
+
+#     def get(self, request, slug):
+#         product = get_object_or_404(Products, slug=slug)
+#         serializer = ProductsSerializers(product)
+#         return Response({'serializer': serializer, 'profile': product})
+
+#     def post(self, request, slug):
+#         product = get_object_or_404(Products, slug=slug)
+#         serializer = ProductsSerializers(product, data=request.data)
+#         if not serializer.is_valid():
+#             return Response({'serializer': serializer, 'product': product})
+#         serializer.save()
+#         return redirect('products')
 
