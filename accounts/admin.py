c0a71c070cce48a795eeda03047d370dfc169ed5
yeazin <naz.yeasin@gmail.com>
2022-06-16 01:27:15 +0600

    update the billing address models

--- accounts/admin.py
+++ accounts/admin.py
@@ -3,11 +3,13 @@ from django.contrib import admin
 from accounts.models.user_model import (
     User
 )
-from accounts.models.profile import Profile,UserPermission
+from accounts.models.profile import (
+    Profile,UserPermission,BillingAddress
+)
 
 admin.site.register(User)
 class ProfileAdmin(admin.ModelAdmin):
     list_display = ['id','full_name','customer_ID','get_permission_id']
 admin.site.register(Profile,ProfileAdmin)
 admin.site.register(UserPermission)
-
+admin.site.register(BillingAddress)
