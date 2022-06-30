d96738598f2247bdc1a0eeffcecaa7cf83bfbfbe
abbappii <bappi42434@gmail.com>
2022-06-30 23:54:53 +0600

    orders url updated

--- orders/urls.py
+++ orders/urls.py
@@ -4,23 +4,26 @@ from orders.views.coupon import (
     CouponDeleteView,CouponEditView
 )
 
-from rest_framework import routers
+# from rest_framework import routers
 from orders.views.cart_order import *
 from orders.views.order_view import (
     OrderView,
     AddOrderItem
 )
-router = routers.DefaultRouter()
-router.register('cart',MyCart,basename="MyCart")
-router.register('orderviewset',OrderViewset,basename='OrderView')
 
-urlpatterns = [
-    path("",include(router.urls)),
-    path("addtocart/",AddtoCartView.as_view(),name="addtocart"),
-    path('updatecart/',Updatecart.as_view(),name='updatecart'),
-    path('editcart/',Editcart.as_view(),name='editcart'),
-    path('deletecart/',DeleteCart.as_view(),name='deleltecart'),
-]
+# router = routers.DefaultRouter()
+# router.register('cart',MyCart,basename="MyCart")
+# router.register('orderviewset',OrderViewset,basename='OrderView')
+
+# urlpatterns = [
+#     path("",include(router.urls)),
+#     path("addtocart/",AddtoCartView.as_view(),name="addtocart"),
+#     path('updatecart/',Updatecart.as_view(),name='updatecart'),
+#     path('editcart/',Editcart.as_view(),name='editcart'),
+#     path('deletecart/',DeleteCart.as_view(),name='deleltecart'),
+# ]
+
+urlpatterns =[]
 
 coupon_URL = [
     path('list/',CouponView.as_view()),
