b57e970daf267ab2ca12e838dcb09526c10c33ee
abbappii <bappi42434@gmail.com>
2022-07-21 20:03:18 +0600

    blog serializer update

--- blog/serializers/blog_serializers.py
+++ blog/serializers/blog_serializers.py
@@ -7,5 +7,6 @@ class BlogSerializers(serializers.ModelSerializer):
 
     class Meta:
         model = Blog
-        fields = ['id','title','description','image','url_field']
+        # fields = ['id','title','description','image','url_field']
+        fields = '__all__'
     
