b3cd2f796db0997732a0c7dd6aaa21e48099de61
yeazin <naz.yeasin@gmail.com>
2022-07-23 21:20:53 +0600

    update on flash sale

--- flash_sale/admin.py
+++ flash_sale/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from flash_sale.models import FlashSale
 
 # Register your models here.
+admin.site.register(FlashSale)
--- flash_sale/models.py
+++ flash_sale/models.py
@@ -1,3 +1,32 @@
-from django.db import models
 
-# Create your models here.
+
+'''
+This file contains the database of 
+    Flash Sale
+'''
+
+from django.db import models
+from accounts.models.initials import InitModels
+
+class FlashSale(InitModels):
+    name = models.CharField(
+        max_length=500,null=True,verbose_name="Flash Sale Name",blank=True
+        )
+    discount = models.PositiveIntegerField(
+        null=True,blank=True,verbose_name="Flash Discount"
+    )
+    start_time = models.DateTimeField(
+        null=True,auto_created=False,verbose_name="Start Time"
+        )
+    end_time = models.DateTimeField(
+        null=True,auto_created=False,verbose_name="End Time"
+    )
+    products = models.ManyToManyField(
+        'products.Products', related_name='flash_products'
+    )
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name_plural = "Flash Deal"
