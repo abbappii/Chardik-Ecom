1dd7187ab779f2762e18cb73c47998b6dbb99aad
yeazin <naz.yeasin@gmail.com>
2022-07-24 17:47:15 +0600

    over ride method in flash sale

--- flash_sale/models.py
+++ flash_sale/models.py
@@ -30,3 +30,17 @@ class FlashSale(InitModels):
 
     class Meta:
         verbose_name_plural = "Flash Deal"
+
+    ## Customer Property 
+    @property
+    def flash_products(self):
+        products = [product.id for product in self.products.all()]    
+        return products
+    ## Override the Save method 
+    # def save(self,*args,**kwargs):
+    #     for product in self.products.all():
+    #         print(product)
+    #     super(FlashSale, self).save(*args, **kwargs) 
+
+    
+        
