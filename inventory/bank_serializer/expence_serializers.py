708476848ba2aff973aa82fec5cd05f8745b9ae6
abbappii <bappi42434@gmail.com>
2022-07-25 17:34:02 +0600

    expence delete url added

--- inventory/bank_serializer/expence_serializers.py
+++ inventory/bank_serializer/expence_serializers.py
@@ -3,13 +3,26 @@ from inventory.bank_model.baccounts import Expenses
 
 from rest_framework import serializers
 
+'''
+Expence serilizers
+        - create api
+        - update api
+'''
 class ExpenceSerializers(serializers.ModelSerializer):
     class Meta:
         model = Expenses
         fields = ['id','name','outlet','reference','account','expence_amount','type','description']
     
  
-
-
+'''
+Expence serializers
+        - list api
+'''
+class ExpenceListSerializers(serializers.ModelSerializer):
+    class Meta:
+        model = Expenses
+        fields = ['id','name','outlet','reference','account','expence_amount','type','description']
+        depth = 1
+ 
 
 
