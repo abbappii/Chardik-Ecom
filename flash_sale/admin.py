79389502ba0480c78f1e35fa9909284eb6c14325
yeazin <naz.yeasin@gmail.com>
2022-07-26 14:57:07 +0600

    update the database and overall

--- flash_sale/admin.py
+++ flash_sale/admin.py
@@ -1,5 +1,8 @@
 from django.contrib import admin
-from flash_sale.models import FlashSale
+from flash_sale.models import (
+    FlashSale,FlashProducts
+)
 
 # Register your models here.
 admin.site.register(FlashSale)
+admin.site.register(FlashProducts)
