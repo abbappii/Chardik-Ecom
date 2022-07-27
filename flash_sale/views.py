a4aae52d651e031c62ec8234745c2a89d35f2cab
yeazin <naz.yeasin@gmail.com>
2022-07-27 21:42:20 +0600

    update in flash sale

--- flash_sale/views.py
+++ flash_sale/views.py
@@ -5,7 +5,10 @@ This file contains the Logics
 '''
 
 # importing Initials 
+from rest_framework import status
+from rest_framework.response import Response
 from rest_framework import generics
+from rest_framework.generics import GenericAPIView
 from rest_framework.decorators import permission_classes
 
 ## importing models 
@@ -50,6 +53,25 @@ class FlashSale_update_deleteView (generics.RetrieveUpdateDestroyAPIView):
     queryset = FlashSale.objects.filter(is_active = True)
     serializer_class = FlashSale_API_func
 
+## Flash sale Inactive functions
+
+class FlashSale_OFF(GenericAPIView):
+
+    def get(self,request,flashID):
+        get_flash_obj = FlashSale.objects.filter(id=flashID)
+        if get_flash_obj:
+
+            for product in get_flash_obj.products.all():
+                product.is_active = False
+                product.save()
+            return Response({
+                'Success':'Flash Sale Obj is Closed'
+            },status=status.HTTP_202_ACCEPTED)
+
+        else:
+            return Response({
+                'Error':'Flash Sale Obj ID didnt match'
+            },status=status.HTTP_404_NOT_FOUND)
 
 
 '''
@@ -61,7 +83,32 @@ Flash Products View Logics
 '''
 
 ## Create View 
-class FlashProducts_createView (generics.CreateAPIView):
+class FlashProducts_createView (GenericAPIView):
     queryset = FlashProducts.objects.filter(is_active = True)
     serializer_class = FlashSale_ProductAPI
+
+    def post(self,request):
+        apifetch = FlashSale_ProductAPI(data=request.data)
+
+        if apifetch.is_valid():
+            obj_check = FlashProducts.objects.\
+                    filter(flash_sale = apifetch.validated_data['flash_sale'],
+                    flash_product=apifetch.validated_data['flash_product'])
+            if obj_check:
+                return Response({
+                    'Error':'Flash Obj Already Exits !'
+                },status=status.HTTP_406_NOT_ACCEPTABLE)
+            else:
+                apifetch.save()
+                return Response({
+                    'Success':'Flash Obj Created'
+                },status=status.HTTP_201_CREATED)
+        else:
+            return Response(apifetch.errors)
+
+
+
+
+
+    
 
