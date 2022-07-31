0f57d06882b9803028590caa91513d6b3a7b37c4
yeazin <naz.yeasin@gmail.com>
2022-08-01 01:33:35 +0600

    orders update bugs fixed

--- inventory/signals.py
+++ inventory/signals.py
@@ -8,7 +8,7 @@ from decimal import Decimal
 @receiver(post_save,sender=Order)
 def order_money_add_to_bank(sender,instance, created,*args,**kwargs):
     if created:
-        bank = BankAccounts.objects.get(bank_name='ORDERS_READ_ONLY')
+        bank = BankAccounts.objects.filter(bank_name='ORDERS_READ_ONLY')
         try:
             bank.amount += Decimal(instance.total)
             bank.save()
