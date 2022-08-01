2535be87db378cdcf0a6236132629855c1f9b2a8
abbappii <bappi142434@gmail.com>
2022-08-01 18:04:35 +0600

    serializers logic

--- products/serializers/feature_products.py
+++ products/serializers/feature_products.py
@@ -5,34 +5,66 @@ from products.database.feature_product import Banner, BannerProduct
 from products.serializers.product_serializers import (
     ProductListAPI
 )
-'''
-feature product 
-        - create 
-        - update 
-'''
-class BannerProductSerializers(serializers.ModelSerializer):
+# '''
+# feature product 
+#         - create 
+#         - update 
+# '''
+class Banner_Product_API(serializers.ModelSerializer):
     class Meta:
         model = BannerProduct
-        fields = ['id','feature_product','banner']
+        fields = [
+            'banner_product',
+            'banner'
+        ]
 
 
-'''
-feature product 
-        - list
-'''
-class BannerProductListSerializers(serializers.ModelSerializer):
-    feature_product = ProductListAPI(read_only=True)
+# '''
+# feature product nested purpose
+#         
+# '''
+class BannerProduct_API_show(serializers.ModelSerializer):
+
+    banner_product = ProductListAPI(read_only=True)
+
     class Meta:
         model = BannerProduct
-        fields = ['id','feature_product','banner','is_active']
-        depth = 1
+        fields = [
+            'banner_product',
+            'is_active'
+        ]
+        
 
+class Banner_API(serializers.ModelSerializer):
+    banner_ID = serializers.ReadOnlyField(source = 'id')
+    products = serializers.SerializerMethodField()
 
-'''
-product filter 
-'''
-# product filter model 
-class BannerSerializers(serializers.ModelSerializer):
     class Meta:
         model = Banner
-        fields = ['id','name','banner_image']
+        fields = [ 
+            'banner_ID',
+            'name',
+            'banner_image',
+            'products'
+        ]
+    
+    # custom method to return the list of banner products 
+    def get_products(self,obj):
+        products_qset = BannerProduct.objects.filter(banner=obj)
+        return [BannerProduct_API_show(m).data for m in products_qset]
+
+'''
+Banner API 
+        - create, 
+        - update, 
+        - delete 
+'''
+
+class Banner_API_func(serializers.ModelSerializer):
+    
+    class Meta:
+        model = Banner
+        fields = [
+            'name',
+            'banner_image',
+        ]
