448964748115b384109cb113d8d8bf8c4dac5bb0
abbappii <bappi142434@gmail.com>
2022-08-08 22:23:29 +0600

    order update prb fix

--- inventory/serializer.py
+++ inventory/serializer.py
@@ -67,14 +67,16 @@ class PurchaseCreateSerializers(serializers.ModelSerializer):
 
     def update(self, instance, validated_data):
 
-       instance.payment_status = validated_data.get('payment_status',instance.payment_status)
-       instance.due_price = validated_data.get('due_price', instance.due_price)
 
-       if instance.payment_status == 'Paid':
-          instance.due_price = 0
-          print('hudai')
-          return instance
-        
+        instance.payment_status = validated_data.get('payment_status',instance.payment_status)
+        instance.due_price = validated_data.get('due_price', instance.due_price)
+
+        if instance.payment_status == 'Paid':
+            instance.due_price = 0
+            print('hudai')
+            return instance
+        else:
+            return instance
 '''
 Define Supplier DUE Reports 
 '''
