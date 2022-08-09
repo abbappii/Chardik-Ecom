04f7403f045231fd378a139d01e31f3aa6b717e4
abbappii <bappi142434@gmail.com>
2022-08-10 01:35:07 +0600

    order ref code generate

--- MainApplication/scripts/ref_code.py
+++ MainApplication/scripts/ref_code.py
@@ -1,28 +1,28 @@
 
-import string
-import random
+# import string
+# import random
 
-from orders.database.cart_order import Order
-import datetime
-date = datetime.date.today()
-s_date = str(date)
+# from orders.database.cart_order import Order
+# import datetime
+# date = datetime.date.today()
+# s_date = str(date)
 
-# order id get 
+# # order id get 
 
-# example = ch20220809(order_id)
+# # example = ch20220809(order_id)
 
-date_str= ''.join(e for e in s_date if e.isalnum())
-code = 'ch' + date_str
+# date_str= ''.join(e for e in s_date if e.isalnum())
+# code = 'ch' + date_str
 
-def random_code(size=10, chars = code + string.digits):
-    return ''.join(random.choice(chars) for _ in range(size))
+# def random_code(size=10, chars = code + string.digits):
+#     return ''.join(random.choice(chars) for _ in range(size))
 
-def unique_refID_generate(instance):
-    r_code = random_code()
-    take = instance.__class__
-    take_exits = take.objects.filter(ref_code=code).exists()
-    if take_exits:
-        return r_code
-    return code
+# def unique_refID_generate(instance):
+#     r_code = random_code()
+#     take = instance.__class__
+#     take_exits = take.objects.filter(ref_code=code).exists()
+#     if take_exits:
+#         return r_code
+#     return code
 
 
